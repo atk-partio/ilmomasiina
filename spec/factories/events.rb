ec9e0a8cfdf2660ed645cb97617985b0f52c0346
Vesa Laakso <vesa.laakso@venuu.fi>
2014-04-19 23:18:35 +0300

    Update events factory annotation

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -2,9 +2,11 @@
 #
 # Table name: events
 #
-#  id         :integer          not null, primary key
-#  created_at :datetime
-#  updated_at :datetime
+#  id          :integer          not null, primary key
+#  created_at  :datetime
+#  updated_at  :datetime
+#  name        :string(255)
+#  description :text
 #
 
 # Read about factories at https://github.com/thoughtbot/factory_girl
