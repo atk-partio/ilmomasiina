63ae9f2f05a1ac1aad8df73c404c4da4ad41b69f
Atte Keinänen <atte.keinanen@gmail.com>
2014-08-30 23:06:47 +0300

    Do not use browser sync

--- frontend/gulp-config/tasks/watch.js
+++ frontend/gulp-config/tasks/watch.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 
-gulp.task('watch', ['useWatchify', 'browserSync'], function() {
+gulp.task('watch', ['useWatchify', 'browserify'], function() {
   gulp.watch('sass/**', ['sass']);
   gulp.watch('images/**', ['images']);
 });
