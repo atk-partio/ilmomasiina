c4330f3328f3745a49edd8c61b6677d02dd5e016
ankku <ankku.lipiainen@gmail.com>
2014-08-30 21:32:21 +0300

    Add validation for presence of event in Question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ActiveRecord::Base
   validates_presence_of :name
+  validates_presence_of :event
+  belongs_to :event
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,4 +3,8 @@ require 'spec_helper'
 RSpec.describe Question, :type => :model do
   # Required attributes
   it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_presence_of :event }
+
+  # Associations
+  it { is_expected.to belong_to :event}
 end
