6bd28c4c185f6e2d833d490eef1944b6debebd52
Atte Keinänen <atte.keinanen@gmail.com>
2014-08-31 00:46:08 +0300

    Fix sass compilation, rename sass folder to scss

--- frontend/gulp-config/tasks/sass.js
+++ frontend/gulp-config/tasks/sass.js
@@ -1,10 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var handleErrors = require('../util/handle-errors');
-var browserSync  = require('browser-sync');
 
 gulp.task('sass', ['images'], function () {
-  return gulp.src('./sass/*.{sass, scss}')
+  return gulp.src('./scss/*.scss')
     .pipe(sass())
     .on('error', handleErrors)
     .pipe(gulp.dest('./../public/'));
--- frontend/gulp-config/tasks/watch.js
+++ frontend/gulp-config/tasks/watch.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 
 gulp.task('watch', ['useWatchify', 'browserify'], function() {
-  gulp.watch('sass/**', ['sass']);
+  gulp.watch('scss/**', ['sass']);
   gulp.watch('images/**', ['images']);
 });
