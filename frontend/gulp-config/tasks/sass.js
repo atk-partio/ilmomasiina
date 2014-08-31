319db68c13ffbfbf8ca130d794e361efab34f4b7
Atte Keinänen <atte.keinanen@gmail.com>
2014-08-31 21:32:55 +0300

    Run autoprefixer after compiling SASS

--- frontend/gulp-config/tasks/sass.js
+++ frontend/gulp-config/tasks/sass.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
 var handleErrors = require('../util/handle-errors');
 
 gulp.task('sass', ['images'], function () {
   return gulp.src('./scss/*.scss')
     .pipe(sass())
+    .pipe(prefix())
     .on('error', handleErrors)
     .pipe(gulp.dest('./../public/'));
 });
