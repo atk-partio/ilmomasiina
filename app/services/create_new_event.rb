336c4f8de867ce81c30008306cd7251561765a27
Vesa Laakso <vesa.laakso@venuu.fi>
2015-02-07 20:48:10 +0200

    Remove confusing service concern

--- app/services/create_new_event.rb
+++ app/services/create_new_event.rb
@@ -1,12 +1,6 @@
 class CreateNewEvent
-  include Service
-
-  def initialize(event_params = nil)
-    @event_params = event_params
-  end
-
-  def call
-    event = Event.new(@event_params)
+  def call(event_params)
+    event = Event.new(event_params)
     event.save
     event
   end
--- spec/services/create_new_event_spec.rb
+++ spec/services/create_new_event_spec.rb
@@ -1,24 +1,23 @@
 require 'spec_helper'
 
 describe CreateNewEvent do
-  it 'includes Service concern' do
-    expect(subject.class.ancestors).to include(Service)
-  end
+  let(:event_params) { Hash.new }
 
   context 'when saving succeeds' do
     let(:event_double) { instance_double("Event", save: true) }
 
     before do
       allow(Event).to receive(:new) { event_double }
+      allow(Question).to receive(:new) { question_double }
     end
 
     it 'creates a new event' do
-      CreateNewEvent.call
+      CreateNewEvent.new.call(event_params)
       expect(event_double).to have_received(:save)
     end
 
     it 'returns the created Event instance' do
-      expect(CreateNewEvent.call).to be(event_double)
+      expect(CreateNewEvent.new.call(event_params)).to be(event_double)
     end
   end
 
@@ -31,7 +30,7 @@ describe CreateNewEvent do
     end
 
     it 'returns the non-persisted AR object' do
-      result = CreateNewEvent.call
+      result = CreateNewEvent.new.call(event_params)
       expect(result).to be(event)
       expect(result).not_to be_persisted
     end
