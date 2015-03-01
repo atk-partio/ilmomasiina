00c3ca6ed843628ced2089884bf0fe78dab924b5
Vesa Laakso <vesa.laakso@venuu.fi>
2015-03-01 20:10:50 +0200

    Make CreateNewEvent work with quota_groups, too

--- app/services/create_new_event.rb
+++ app/services/create_new_event.rb
@@ -1,8 +1,10 @@
 class CreateNewEvent
   def call(event_params)
     questions_params = event_params.delete(:questions) || []
+    quota_groups_params = event_params.delete(:quota_groups) || []
     event = Event.new(event_params)
-    event.questions = questions_params.map { |q| Question.new(q) }
+    event.questions = questions_params.map { |q_params| Question.new(q_params) }
+    event.quota_groups = quota_groups_params.map { |qg_params| QuotaGroup.new(qg_params) }
     event.save
     event
   end
--- spec/services/create_new_event_spec.rb
+++ spec/services/create_new_event_spec.rb
@@ -2,13 +2,16 @@ require 'spec_helper'
 
 describe CreateNewEvent do
   let(:event_params) { Hash.new }
+  let(:question_double) { instance_double("Question").as_null_object }
+  let(:event_double) { instance_double("Event").as_null_object }
+  let(:quota_group_double) { instance_double("QuotaGroup").as_null_object }
 
   context 'when saving succeeds' do
-    let(:event_double) { instance_double("Event").as_null_object }
 
     before do
       allow(Event).to receive(:new) { event_double }
       allow(Question).to receive(:new) { question_double }
+      allow(QuotaGroup).to receive(:new) { quota_group_double }
     end
 
     it 'creates a new event' do
@@ -21,7 +24,6 @@ describe CreateNewEvent do
     end
 
     describe 'with questions attribute' do
-      let(:question_double) { instance_double("Question").as_null_object }
       let(:question_params) { attributes_for_list(:question, 2) }
 
       it 'creates a new question for each question' do
@@ -29,6 +31,17 @@ describe CreateNewEvent do
         expect(event_double).to receive(:questions=).with(satisfy { |a| a.length == question_params.length })
 
         CreateNewEvent.new.call(event_params.merge(questions: question_params))
+      end
+    end
+
+    describe 'with quota_groups attribute' do
+      let(:quota_group_params) { attributes_for_list(:quota_group, 3) }
+
+      it 'creates a new quota group for each quota group' do
+        expect(QuotaGroup).to receive(:new).exactly(quota_group_params.length).times { quota_group_double }
+        expect(event_double).to receive(:quota_groups=).with(satisfy { |a| a.length == quota_group_params.length })
+
+        CreateNewEvent.new.call(event_params.merge(quota_groups: quota_group_params))
       end
     end
   end
