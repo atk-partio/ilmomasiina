876e31c690c77ba57cdc27c95bffb9585df4b7d2
Atte Keinänen <atte.keinanen@gmail.com>
2015-03-04 21:42:04 +0200

    Add a simple way to switch between real API and example data

--- app/assets/javascripts/utils/API.js
+++ app/assets/javascripts/utils/API.js
@@ -1,9 +1,21 @@
 import request from 'superagent';
 
+var useExampleData = false;
+
+var realEndpoints = {
+  'getEvent': (eventId) => `/api/events/${eventId}`
+};
+
+var exampleEndpoints = {
+  'getEvent': (eventId) => `/examples/events/${eventId}.json`
+};
+
+var endpoints = useExampleData ? exampleEndpoints : realEndpoints;
+
 var API = {
   loadEvent(eventId) {
     return new Promise((resolve, reject) => {
-      request.get(`/api/events/${eventId}`, function(res) {
+      request.get(endpoints.getEvent(eventId), function(res) {
         if (res.ok) {
           resolve(res.body);
         }
